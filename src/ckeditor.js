e1af2e4b47b6c79d6e87ec7faa07a6820d8d05ae
AJL <luc.bayo@gmail.com>
2022-12-01 21:10:09 +0200

    :rocket: initial commit

---
+++ src/ckeditor.js
@@ -0,0 +1,44 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	BlockQuote,
+	Bold,
+	Essentials,
+	Indent,
+	IndentBlock,
+	Italic,
+	List,
+	ListProperties,
+	Paragraph,
+	WordCount
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'|',
+			'|'
+		]
+	},
+	language: 'de'
+};
+
+export default Editor;
